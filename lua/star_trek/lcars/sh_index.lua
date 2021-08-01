ac504199be2076c106e0a9428be903452616fb93
Oninoni <jan@cheap-purple.com>
2021-08-01 15:36:11 +0200

    Removed Button Offset Config

--- lua/star_trek/lcars/sh_index.lua
+++ lua/star_trek/lcars/sh_index.lua
@@ -21,7 +21,6 @@ Star_Trek:RequireModules("util")
 Star_Trek.LCARS = Star_Trek.LCARS or {}
 
 if SERVER then
-	AddCSLuaFile("sh_config.lua")
 	AddCSLuaFile("sh_colors.lua")
 	AddCSLuaFile("sh_sounds.lua")
 	AddCSLuaFile("cl_fonts.lua")
@@ -31,7 +30,6 @@ if SERVER then
 	AddCSLuaFile("sh_lcars.lua")
 	AddCSLuaFile("cl_lcars.lua")
 
-	include("sh_config.lua")
 	include("sh_colors.lua")
 	include("sh_sounds.lua")
 	include("sv_util.lua")
@@ -40,7 +38,6 @@ if SERVER then
 end
 
 if CLIENT then
-	include("sh_config.lua")
 	include("sh_colors.lua")
 	include("sh_sounds.lua")
 	include("cl_fonts.lua")
