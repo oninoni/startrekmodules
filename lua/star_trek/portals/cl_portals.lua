f2344143af975e18312cb4929802fb82004ee0a6
Oninoni <jan@cheap-purple.com>
2021-08-16 18:05:28 +0200

    Improved Portal Code

--- lua/star_trek/portals/cl_portals.lua
+++ lua/star_trek/portals/cl_portals.lua
@@ -21,30 +21,9 @@ hook.Add("wp-shouldrender", "Star_Trek.HidePortalInDoors", function(portal, exit
 	local camOrigin = LocalPlayer():EyePos()
 	local distance = camOrigin:Distance( portal:GetPos() )
 	local disappearDist = portal:GetDisappearDist()
-
-	if portal:GetClass() == "linked_portal_window" then return end
-
 	if not (disappearDist <= 0) and distance > disappearDist then return false end
 
-	local parent = portal:GetParent()
-	if IsValid(parent) then
-		local sequenceId = parent:GetSequence()
-		if parent.LastSequence ~= sequenceId then
-			if sequenceId == parent:LookupSequence("close") then
-				parent.DelayRenderDisable = CurTime() + parent:SequenceDuration("close") * 2
-			end
-
-			parent.LastSequence = sequenceId
-		end
-
-		if sequenceId == parent:LookupSequence("open") then
-			return
-		end
-
-		if isnumber(parent.DelayRenderDisable) and parent.DelayRenderDisable > CurTime() then
-			return true
-		end
-
+	if Star_Trek.Portals:IsBlocked(portal) then
 		return false, true
 	end
 end)
---
+++ lua/star_trek/portals/sh_portals.lua
@@ -0,0 +1,53 @@
+---------------------------------------
+---------------------------------------
+--        Star Trek Utilities        --
+--                                   --
+--            Created by             --
+--       Jan 'Oninoni' Ziegler       --
+--                                   --
+-- This software can be used freely, --
+--    but only distributed by me.    --
+--                                   --
+--    Copyright © 2020 Jan Ziegler   --
+---------------------------------------
+---------------------------------------
+
+---------------------------------------
+--          Portals | Shared         --
+---------------------------------------
+
+function Star_Trek.Portals:IsBlocked(portal)
+	local parent = portal:GetParent()
+	if IsValid(parent) then
+		local sequenceId = parent:GetSequence()
+		if parent.LastSequence ~= sequenceId then
+			if sequenceId == parent:LookupSequence("close") then
+				parent.DelayRenderDisable = CurTime() + parent:SequenceDuration("close") * 2
+			end
+
+			parent.LastSequence = sequenceId -- This needs to go!
+		end
+
+		if sequenceId == parent:LookupSequence("open") then
+			return false
+		end
+
+		if isnumber(parent.DelayRenderDisable) and parent.DelayRenderDisable > CurTime() then
+			return false
+		end
+
+		return true
+	end
+
+	return false
+end
+
+hook.Add("wp-trace", "doors-portals", function(portal)
+	if portal:GetClass() == "linked_portal_window" then
+		return false
+	end
+
+	if Star_Trek.Portals:IsBlocked(portal) then
+		return false
+	end
+end)
