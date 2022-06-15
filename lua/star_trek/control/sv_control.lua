45c5c24f0274aa283912947a5d473df63d9e89df
Oninoni <jan@cheap-purple.com>
2022-06-15 16:39:28 +0200

    Rework Control System

--- lua/star_trek/control/sv_control.lua
+++ lua/star_trek/control/sv_control.lua
@@ -21,16 +21,35 @@ Star_Trek.Control.ACTIVE      = 1
 Star_Trek.Control.INOPERATIVE = 2
 
 -- Register a Control Type.
+--
 -- @param String name
-function Star_Trek.Control:Register(name)
+function Star_Trek.Control:Register(name, callback)
 	Star_Trek.Control.Types = Star_Trek.Control.Types or {}
 
 	local controlType = {}
+	controlType.Callback = callback
 
 	Star_Trek.Control.Types[name] = controlType
 end
 
+-- Clean up all Sections on a deck, that are not broken.
+--
+-- @param Table deckData
+local function cleanupDeck(deckData)
+	local sectionIds = {}
+	for sectionId, value in ipairs(deckData) do
+		if value ~= Star_Trek.Control.INOPERATIVE then
+			table.insert(sectionIds, sectionId)
+		end
+	end
+
+	for _, sectionId in pairs(sectionIds) do
+		deckData[sectionId] = nil
+	end
+end
+
 -- Set the status of a control type.
+--
 -- @param String name
 -- @param Number value
 -- @param? Number deck
@@ -43,9 +62,20 @@ function Star_Trek.Control:SetStatus(name, value, deck, sectionId)
 		return false, "Invalid Control Type"
 	end
 
-	if  value ~= Star_Trek.Control.INACTIVE
-	and value ~= Star_Trek.Control.ACTIVE then
-		return false, "Invalid Value"
+	if isnumber(deck) and isnumber(sectionId) then
+		local success, sectionData = Star_Trek.Sections:GetDeck(deck, sectionId)
+		if not success then
+			return false, sectionData
+		end
+
+		controlType[deck] = controlType[deck] or {}
+		controlType[deck][sectionId] = value
+
+		if isfunction(controlType.Callback) then
+			controlType.Callback(value, deck, sectionId)
+		end
+
+		return true
 	end
 
 	if isnumber(deck) then
@@ -54,34 +84,40 @@ function Star_Trek.Control:SetStatus(name, value, deck, sectionId)
 			return false, deckData
 		end
 
-		if isnumber(sectionId) then
-			local success2, sectionData = Star_Trek.Sections:GetDeck(deck, sectionId)
-			if not success2 then
-				return false, sectionData
-			end
+		-- Clean up Sections Stuff.
+		cleanupDeck(deckData)
 
-			controlType[deck] = controlType[deck] or {}
-			controlType[deck][sectionId] = value
-
-			return true
-		end
-
-		controlType[deck] = {}
+		controlType[deck] = controlType[deck] or {}
 		controlType[deck].Value = value
+
+		if isfunction(controlType.Callback) then
+			controlType.Callback(value, deck)
+		end
 
 		return true
 	end
 
-	controlType = {}
 	controlType.Value = value
+	for _, deckData in ipairs(controlType) do
+		-- Clean up Deck Wide Stuff.
+		if deckData.Value ~= Star_Trek.Control.INOPERATIVE then
+			deckData.Value = nil
+		end
 
-	Star_Trek.Control.Types[name] = controlType
+		-- Clean up Sections Stuff.
+		cleanupDeck(deckData)
+	end
+
+	if isfunction(controlType.Callback) then
+		controlType.Callback(value)
+	end
 
 	return true
 end
 
 -- Get the status of a control type.
 -- If the deck and sectionId is given it checks the specified area, falling back to a lower level if not set.
+--
 -- @param String name
 -- @param? Number deck
 -- @param? Number sectionId
