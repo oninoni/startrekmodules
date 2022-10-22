f5b6d7d22c66778059a743d3e3259139f35f9b64
Oninoni <jan@cheap-purple.com>
2022-10-22 18:18:43 +0200

    Move Engineering Extension to other Addon

--- lua/star_trek/logs/sh_index.lua
+++ lua/star_trek/logs/sh_index.lua
@@ -16,7 +16,7 @@
 --            Logs | Index           --
 ---------------------------------------
 
-Star_Trek:RequireModules("lcars", "button", "sections")
+Star_Trek:RequireModules("lcars", "sections")
 
 Star_Trek.Logs = Star_Trek.Logs or {}
 
@@ -30,27 +30,3 @@ end
 if CLIENT then
 	return
 end
-
-if game.GetMap() ~= "rp_intrepid_v1" then return end
-
-local setupButton = function()
-	if IsValid(Star_Trek.Logs.Button) then
-		Star_Trek.Logs.Button:Remove()
-	end
-
-	local eng_security = ents.FindByName("gb60")[1]
-	local pos = eng_security:GetPos()
-	local ang = eng_security:GetAngles()
-
-	pos = pos + ang:Right() * 96 + ang:Forward() * 30
-	ang = ang + Angle(0, 123.5, 90)
-
-	local success, ent = Star_Trek.Button:CreateInterfaceButton(pos, ang, "models/hunter/blocks/cube125x125x025.mdl", "eng_logs")
-	if not success then
-		print(ent)
-	end
-	Star_Trek.Logs.Button = ent
-end
-
-hook.Add("InitPostEntity", "Star_Trek.Logs.SpawnButton", setupButton)
-hook.Add("PostCleanupMap", "Star_Trek.Logs.SpawnButton", setupButton)
