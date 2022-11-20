625a3ad29184e9739c96eb24f2cd732d8fb6e6f2
GuuscoNL <99217547+GuuscoNL@users.noreply.github.com>
2022-11-20 22:10:32 +0100

    Revert "Fix Overlapping ENG console"
    This reverts commit 4b48861acc1765e51b1cb9c16462ff0d54d424b3.

--- lua/star_trek/transporter/interfaces/eng_transporter/init.lua
+++ lua/star_trek/transporter/interfaces/eng_transporter/init.lua
@@ -28,7 +28,7 @@ SELF.AdvancedMode = false
 function SELF:Open(ent)
 	local success, windows = self:OpenInternal(
 		{
-			Pos = Vector(-12.02, 3, -14.25),
+			Pos = Vector(-12, 3, -14.25),
 			Ang = Angle(0, 0, 0),
 			Width = 580,
 		},
